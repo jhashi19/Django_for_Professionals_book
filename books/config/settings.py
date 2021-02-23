fc182f5a68400aa28b6084d60a3746639c1b21ec
jhashi19 <j.hashi19@gmail.com>
2021-02-23 16:14:05 +0900

    ch12

--- books/config/settings.py
+++ books/config/settings.py
@@ -158,6 +158,9 @@ STATICFILES_FINDERS = [
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
 ]
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = str(BASE_DIR.joinpath('media'))
+
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 AUTH_USER_MODEL = 'accounts.CustomUser'
